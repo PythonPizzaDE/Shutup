83561e7e6862f1a223756c807effa270e9dfa7fd
not my name <not@email.com>
2022-07-07 16:10:07 +0200

    added UI

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,49 @@
+use gtk::prelude::*;
+use gtk::{Application, ApplicationWindow, Button, Orientation, ButtonBox, ButtonBoxStyle};
+
 fn main() {
-    println!("Hello, world!");
+    let app = Application::builder()
+        .application_id("org.pythonpizza.Shutup")
+        .build();
+    
+    app.connect_activate(|app| {
+        let window = ApplicationWindow::builder()
+            .application(app)
+            .resizable(false)
+            .default_width(1000)
+            .default_height(300)
+            .title("Shutup")
+            .build();
+
+        let button_box = ButtonBox::builder()
+            .orientation(Orientation::Horizontal)
+            .layout_style(ButtonBoxStyle::Expand)
+            .build();
+
+        let shutdown_button = Button::with_label("Shutdown");
+        let restart_button = Button::with_label("Reboot");
+        let logout_button = Button::with_label("Logout");
+
+        button_box.add(&shutdown_button);
+        button_box.add(&restart_button);
+        button_box.add(&logout_button);
+
+        shutdown_button.connect_clicked(|_| {
+            eprintln!("Clicked!");
+        });
+
+        restart_button.connect_clicked(|_| {
+            eprintln!("Clicked!");
+        });
+
+        logout_button.connect_clicked(|_| {
+            eprintln!("Clicked!");
+        });
+
+        window.add(&button_box);
+
+        window.show_all();
+    });
+
+    app.run();
 }
